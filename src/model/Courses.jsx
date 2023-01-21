1fae6b2fe16dd89537ab1fcece1e6ebb9b3fcd2c
bmasmhj <bmasmhj@gmail.com>
2023-01-21 20:50:36 +0545

    random courses

--- src/model/Courses.jsx
+++ src/model/Courses.jsx
@@ -1,7 +1,23 @@
 import { NavLink } from "react-router-dom";
-
+import { useState , useEffect } from "react";
+import Instance from "../axios";
 
 function Courses() {
+    const [popularlist , setpopularcourse] = useState([]);
+
+
+    const pupularcourses = async () => {
+        try {
+            const res_course = await Instance.get(`/popular.php`);
+            setpopularcourse(res_course.data);
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
+    useEffect(() => {
+        pupularcourses();
+    },[]);
+
   return (
     <>
         <section className="m-0 py-5 bg-white position-relative">
@@ -12,18 +28,25 @@ function Courses() {
                             <div className="row text-center justify-content-center">
                                 <div className="col-lg-6">
                                     <h3 className="mt-4">
-                                        <span className="text-gradient text-info">Popular Courses</span>
+                                        <span className="text-gradient text-info">Random Courses</span>
                                     </h3>
-                                    <p className="lead">We're constantly trying to express ourselves and actualize our
-                                        dreams. If you have
-                                        the
-                                        opportunity to play this game of life you need to appreciate every moment.</p>
+                                    <p className="lead">Getting hard to choose course ? This might help you.. Else just reload and see what's next.. <br/> Orrr just click see more to check out all courses and videos</p>
                                 </div>
                             </div>
                             <div className="row mt-5" id="popcourses">
-                                    <PopularCourse/>
+                                {
+                                popularlist.map((resdata)=>{
+                                        return(
+                                            <>
+                                                <PopularCourse 
+                                                    data={resdata}
+                                                />
+                                            </>
+                                        )
+                                })
+                                }
                             </div>
-                            <span>see more</span>
+                            <h6 className="text-center"><NavLink to="/AllCourse">See more</NavLink></h6>
                         </div>
                     </section>
                 </div>
@@ -33,16 +56,16 @@ function Courses() {
   );
 }
 
-function PopularCourse(){
+function PopularCourse({data}){
      return (
     <>
-        <NavLink to="Video/cloud-functions-master-course"  data-aos="fade-up"  className="col-lg-3 mb-3 ">
+        <NavLink to={`Video/`+data.course_id}  data-aos="fade-up"  className="col-lg-3 mb-3 ">
             <div className="card bg-transparent move-on-hover">
                 <div className="card-body p-0">
-                    <img className="w-100" src="https://learn.bimash.com.np/assets/img/thumbnails/cloud-functions-master-course-02-what-is-serverless.png" alt="course title" />
+                    <img className="w-100" src={data.thumbnail} alt="course title" />
                 </div>
             </div>
-            <h6 className="p-3">Course Title</h6>
+            <h6 className="p-3">{data.name}</h6>
         </NavLink>
     </>
   );
