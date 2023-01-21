41c2dcd9a0a5d2ff21628e79ae7cad634913b06a
bmasmhj <bmasmhj@gmail.com>
2023-01-21 20:54:10 +0545

    move on hover

--- src/model/VideoPlayer/VideoPreview.jsx
+++ src/model/VideoPlayer/VideoPreview.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
-function VideoPreview({currElem , uniqid }){
+function VideoPreview({currElem}){
     return(
         <>
          <NavLink data-aos='fade-in' to={`/video/${currElem.video_uri}`} className="pb-2 mb-3 col-md-4 col-sm-6 col-12 col-xl-3 ">
-          <div className="video_thumbnails">
-            <img alt='course name' className="card w-100 preview p-0  col-5" src={currElem.thumbnail}  />
-            <h6 className="m-0 p-2">{currElem.title}</h6>
-          </div>
-
+           <div className="card bg-transparent move-on-hover">
+                <div className="card-body p-0">
+                    <img className="w-100" src={currElem.thumbnail} alt="course title" />
+                </div>
+            </div>
+            <h6 className="p-3">{currElem.title}</h6>
         </NavLink> 
         </>
     )
