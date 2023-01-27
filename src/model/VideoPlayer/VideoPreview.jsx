a0e22628a6c9de89ea768de5f688afa823347e40
bmasmhj <bmasmhj@gmail.com>
2023-01-27 13:26:35 +0545

    video preview mode changed

--- src/model/VideoPlayer/VideoPreview.jsx
+++ src/model/VideoPlayer/VideoPreview.jsx
@@ -5,7 +5,7 @@ function VideoPreview({currElem}){
          <NavLink data-aos='fade-in' to={`/video/${currElem.video_uri}`} className="pb-2 mb-3 col-md-4 col-sm-6 col-12 col-xl-3 ">
            <div className="card bg-transparent move-on-hover">
                 <div className="card-body p-0">
-                    <img className="w-100" src={currElem.thumbnail} alt="course title" />
+                    <div className="showcaseimg" style={{background: `url(${currElem.thumbnail})`}}/>
                 </div>
             </div>
             <h6 className="p-3">{currElem.title}</h6>
