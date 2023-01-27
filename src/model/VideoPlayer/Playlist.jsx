3cb5ba17cfd5296755a891028d93a46afe4ee9a7
bmasmhj <bmasmhj@gmail.com>
2023-01-27 13:25:54 +0545

    highlight now playing

--- src/model/VideoPlayer/Playlist.jsx
+++ src/model/VideoPlayer/Playlist.jsx
@@ -7,7 +7,7 @@ function PlayList({currElem }){
             <img alt='course name' className="card p-0 f-xh col-5" src={currElem.thumbnail}  />
             <div className="col-7 d-flex flex-column justify-space-between">
                 <h6 className="m-0  ">{currElem.title}</h6>
-                <p className="p-0 pb-3 m-0 d-flex align-items-end  justify-content-between"><span> <i className="fa-duotone fa-timer"></i>{currElem.duration} Duration </span> <br/>  <span>{currElem.views} views </span></p>
+                <p className=" playing p-0 pb-3 m-0 d-flex align-items-end  justify-content-between"><span> <i className="fa-duotone fa-timer"></i> {currElem.duration} Duration </span> <br/>  <span>{currElem.views} views </span></p>
             </div>
         </NavLink> 
         <hr/>
